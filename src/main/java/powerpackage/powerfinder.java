a19d5a458f9124105eb51cd49d0802b5222ddfd1
Sunandhita <149308660+Sunandhita@users.noreply.github.com>
2024-07-29 02:45:36 +0530

    Update powerfinder.java

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,11 +1,20 @@
 package powerpackage;
 
-public class powerfinder {
-    public static int OF(int xyz, int pqr) {
+public class PowerFinder //Class name should start with upper case letter
+{
+	//Method name should start with lower case letter
+	
+	/*Also, it is better if methods have meaningful names, 
+	this way it is easier to understand what the method does*/
+	
+    public static int powerOf(int xyz, int pqr)
+    {
         int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
+        for (int i = 0; i<pqr; i++) 
+        {
+            p*=xyz;
         }
         return p;
     }
 }
+
